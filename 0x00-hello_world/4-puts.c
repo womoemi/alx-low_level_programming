4b7a65e06adf1fa26b6b9eb5a022fcf8d49f13e1
womoemi <111122332+womoemi@users.noreply.github.com>
2022-09-09 23:27:34 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry point
  * Return: Always 0 (success)
- * */
+ */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
