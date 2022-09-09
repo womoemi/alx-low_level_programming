5f9d83e269c35348230f90fb23dfd263a14f37f7
womoemi <111122332+womoemi@users.noreply.github.com>
2022-09-09 23:33:32 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	printf("size of a char: %d byte(s)\n", (int) sizeof(chr));
+	printf("size of a char: %d byte(s)\n", (int) sizeof(char));
 	printf("size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("size of a long int: %d byte(s)\n", (int) sizeof(long long int));
 	pintf("size of a float: %d byte(s)\n", (int) sizeof(float));
