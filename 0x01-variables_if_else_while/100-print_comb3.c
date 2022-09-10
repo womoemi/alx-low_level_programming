dce1e37f7134cf9a231597d1b07c5acd0f8fef9d
root <bolouwomoemi@gmail.com>
2022-09-10 12:55:53 -0700

    inventing is a combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = '0'; i <= '9'; c++)
+	{
+		if (i = '0'; i <= '9'; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
