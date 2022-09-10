78a569f802f907832f7bef48ea6ff522d195d305
root <bolouwomoemi@gmail.com>
2022-09-10 13:12:20 -0700

    success combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints 3 combination of numbers
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (a != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
