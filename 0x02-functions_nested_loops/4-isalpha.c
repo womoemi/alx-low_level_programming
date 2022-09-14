f635d371e3fef9680cd03e05683010aa34171f44
root <bolouwomoemi@gmail.com>
2022-09-14 15:57:14 -0700

    isalpha task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabet characters
+ * @c: This is an ACII character
+ *
+ * Return: 0 if not an alphabet, 1 if alphabet
+ */
+int _isalpha(innt c)
+
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}	
