022c16a6949a03cc7b2d0a5c82eb86cc626cc2d8
root <bolouwomoemi@gmail.com>
2022-09-14 17:19:54 -0700

    a+b task 10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,24 +14,3 @@ void times_table(void)
 		for (nn = 0; nn <= 9; nn++)
 		{
 			mul = mm * nn;
-			if (nn == 0)
-				_putchar('0' + mul);
-			else if (mul < 10)
-			{
-				_putchar(' ' );
-				_putchar('0' + mul);
-			}
-			else
-			{
-				_putchar('0' + mul / 10);
-				_putchar('0' + mul % 10);
-			}
-			if (nn < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
-}
