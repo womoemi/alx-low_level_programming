b2b256a669998b65d170949b658c47c7a824c848
Woyengi-womoemi Bolou <111122332+womoemi@users.noreply.github.com>
2022-09-28 22:29:30 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string recursively in reverse
- * @s: This is the string
- *
- * Return: void
+ * _print_rev_recursion - reverse
+ * @s: pointer to string params
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		_Putchar(*s);
+		_putchar(*s);
 	}
 }
