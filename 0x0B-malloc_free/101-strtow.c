d63f0f1819a8dd431ce21362877850ff691d0e09
Woyengi-womoemi Bolou <111122332+womoemi@users.noreply.github.com>
2022-10-04 14:21:37 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -35,7 +35,7 @@ int count_words(char *str)
 {
 	int index = 0, words = 0, len = 0;
 
-	for (insex = 0; *(str + index); index++)
+	for (index = 0; *(str + index); index++)
 		len++;
 
 	for (index = 0; index < len; index++)
