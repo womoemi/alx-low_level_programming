f7001df779d15f6edb51487e304e627e67d60f55
Woyengi-womoemi Bolou <111122332+womoemi@users.noreply.github.com>
2022-10-20 19:20:19 +0100

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -7,11 +7,11 @@ section .data
         globl main
 
 main:
-        push   rbp
-        mov    rsi, msg
-    	mov    rdi, fmt
-	mov    rax, 0
-	call   printf
-	pop    rbp
-	mov    rax, 0
+        push    rbp
+        mov     rsi, msg
+    	mov     rdi, fmt
+	mov     rax, 0
+	call    printf
+	pop     rbp
+	mov     rax, 0
 	ret
