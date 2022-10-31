91ecffc86d68e9aea7336c442785d0ed77e442db
Woyengi-womoemi Bolou <111122332+womoemi@users.noreply.github.com>
2022-10-31 17:57:29 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,8 +6,8 @@
  * @text_content: The string to add to the end of the file.
  *
  * Return: If the function fails or filename is NULL -1
- *     	if the file does not exist the user lacks write permissions -1
- *     	otherwise 1
+ *	if the file does not exist the user lacks write permissions -1
+ *	otherwise 1
  */
 
 int append_text_to_file(const char *filename, char *text_content)
